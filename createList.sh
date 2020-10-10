84c8c2d16f0c4f2f9ca853dfbd5aa6067882fc3a
fire1ce <stas@3os.org>
2020-10-10 17:47:27 +0300

    _update

--- createList.sh
+++ createList.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Sets Working Dir as Real A Script Location
+if [ -z $(which realpath) ]; then
+    brew install coreutils
+fi
+cd $(dirname "$(realpath "$0")")
+
+# Pull adn Create List
 for source in $(cat lists.lst); do
     echo $source
     curl --silent $source >>rawList.txt
@@ -14,3 +22,8 @@ echo -e "\nRemoving duplicates..."
 sort -u parsedList.txt >uniqueList.txt
 rm parsedList.txt
 echo -e "\t$(wc -l uniqueList.txt | cut -d " " -f 1) lines after deduping"
+
+# Pushing to Repo
+git add . 2>&1
+git commit -m "${DATE}_update" 2>&1
+git push 2>&1
